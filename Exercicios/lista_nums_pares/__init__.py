1d14ff0c3d5a5398e4615ddef02c8e4c647991cd
tex-andre <39646908+tex-andre@users.noreply.github.com>
2018-09-03 23:55:37 +0100

    novos exercicios
    3/9/18

---
+++ Exercicios/lista_nums_pares/__init__.py
@@ -0,0 +1,5 @@
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+b = [num for num in a if(not num % 2)]
+
+print(b)
---
+++ Exercicios/pedra_papel_tesoura/__init__.py
@@ -0,0 +1,80 @@
+import sys
+
+class Jogador:
+    def __init__(self, nome):
+        self.nome = nome
+        self.jogada = ""
+        
+    def Jogada(self, escolha):
+        self.jogada = escolha
+        
+def intro():
+    print("Jogo pedra papel ou tesoura!\n")
+    
+def novo_jogador():
+    nome = input("Nome: ")
+    jogador = Jogador(nome)
+    return jogador
+
+def parabens(jogador):
+    print("Parabens %s!" % jogador.nome)
+
+def jogo(jogador1, jogador2):
+    fim_de_jogo = 0
+    while not fim_de_jogo:
+        print("Vez do jogador 1:\n")
+        jogador1.Jogada(input("Introduza a sua escolha: "))
+        print("Vez do jogador 2:\n")
+        jogador2.Jogada(input("Introduza a sua escolha: "))
+        
+        if vencedor(jogador1, jogador2) == 1:
+            parabens(jogador1)
+            fim_de_jogo = 1
+        elif vencedor(jogador1, jogador2) == 2:
+            parabens(jogador2)
+            fim_de_jogo = 1
+        else:
+            print("Empate!\n")
+    
+    
+def vencedor(jogador1, jogador2):
+    if jogador1.jogada == "pedra":
+        if jogador2.jogada == "tesoura":
+            return 1
+        elif jogador2.jogada == "papel":
+            return 2
+        else:
+            return 0
+    elif jogador1.jogada == "tesoura":
+        if jogador2.jogada == "papel":
+            return 1
+        elif jogador2.jogada == "pedra":
+            return 2
+        else:
+            return 0
+    elif jogador1.jogada == "papel":
+        if jogador2.jogada == "pedra":
+            return 1
+        elif jogador2.jogada == "tesoura":
+            return 2
+        else:
+            return 0
+    else:
+        print("ERRO: opcao invalida!\n")
+        sys.exit(1)
+    
+def outro():
+    return int(input("Outro jogo?\n1 se SIM, 0 se NAO: "))
+
+if __name__ == "__main__":
+    intro()
+    print("Jogador 1:\n")
+    jogador1 = novo_jogador()
+    print("Jogador 2:\n")
+    jogador2 = novo_jogador()
+    
+    jogo(jogador1, jogador2)
+    while outro():
+        jogo(jogador1, jogador2)
+    else:
+        exit(0)
